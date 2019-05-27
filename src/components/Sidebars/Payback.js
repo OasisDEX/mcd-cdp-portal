9fc7f9a49b8559987df78b88f83fd53724685af2
sirromdev <sirromdev@gmail.com>
2019-05-27 15:30:00 +0100

    implemented set allowance option

--- src/components/Sidebars/Payback.js
+++ src/components/Sidebars/Payback.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Text, Input, Grid, Link, Button } from '@makerdao/ui-components-core';
+import {
+  Box,
+  Text,
+  Input,
+  Grid,
+  Link,
+  Button,
+  Toggle
+} from '@makerdao/ui-components-core';
 import SidebarActionLayout from 'layouts/SidebarActionLayout';
 import useMaker from '../../hooks/useMaker';
 import {
@@ -11,6 +19,7 @@ import Info from './shared/Info';
 import InfoContainer from './shared/InfoContainer';
 import lang from 'languages';
 import { MAX_UINT_BN } from '../../utils/units';
+import Loader from '../Loader';
 
 const Payback = ({ cdp, reset }) => {
   const { maker, newTxListener } = useMaker();
@@ -18,11 +27,72 @@ const Payback = ({ cdp, reset }) => {
   const [daiBalance, setDaiBalance] = useState(0);
   const [liquidationPrice, setLiquidationPrice] = useState(0);
   const [collateralizationRatio, setCollateralizationRatio] = useState(0);
+  const [allowanceState, setAllowanceState] = useState({
+    proxyAddress: '',
+    userStartedWithoutAllowance: false,
+    allowance: null,
+    loading: false
+  });
+
+  useEffect(() => {
+    const checkProxy = async () => {
+      const proxy = await maker.service('proxy').ensureProxy();
+      return proxy;
+    };
+
+    const init = async () => {
+      const proxy = await checkProxy();
+      const allowance = await checkAllowance({ address: proxy });
+      setAllowanceState({
+        ...allowanceState,
+        proxyAddress: proxy,
+        userStartedWithoutAllowance: !allowance,
+        allowance
+      });
+    };
+    init();
+  }, []);
+
+  const checkAllowance = async ({ address }) => {
+    const daiToken = maker.getToken('MDAI');
+    return (await daiToken.allowance(maker.currentAddress(), address)).eq(
+      MAX_UINT_BN
+    );
+  };
+
+  const setAllowance = async () => {
+    // if allowance is already set, do nothing
+    if (allowanceState.allowance) return;
+
+    try {
+      setAllowanceState({
+        ...allowanceState,
+        loading: true
+      });
+      const daiToken = maker.getToken('MDAI');
+      const { proxyAddress: address } = allowanceState;
+      await daiToken.approveUnlimited(address);
+      const allowance = await checkAllowance({ address });
+      if (allowance) {
+        setAllowanceState({
+          ...allowanceState,
+          allowance,
+          loading: false
+        });
+      } else {
+        setAllowanceState({ ...allowanceState, loading: false });
+      }
+    } catch (e) {
+      setAllowanceState({ ...allowanceState, loading: false });
+    }
+  };
 
   maker
     .getToken('MDAI')
     .balance()
     .then(daiBalance => setDaiBalance(daiBalance.toNumber()));
+
+  const setMax = () => setAmount(Math.min(cdp.debt.toNumber(), daiBalance));
 
   useEffect(() => {
     const amountToPayback = parseFloat(amount || 0);
@@ -36,30 +106,18 @@ const Payback = ({ cdp, reset }) => {
     setCollateralizationRatio(collateralizationRatio);
   }, [amount, cdp.collateral, cdp.debt, cdp.ilkData]);
 
-  const setMax = () => setAmount(Math.min(cdp.debt.toNumber(), daiBalance));
-
-  const payback = async () => {
-    const proxyAddress = await maker.service('proxy').ensureProxy();
-    const daiToken = maker.getToken('MDAI');
-
-    const daiAllowanceSet = (await daiToken.allowance(
-      maker.currentAddress(),
-      proxyAddress
-    )).eq(MAX_UINT_BN);
-
-    if (!daiAllowanceSet) {
-      await daiToken.approveUnlimited(proxyAddress);
-    }
-
+  const payback = () => {
     newTxListener(cdp.wipeDai(parseFloat(amount)), 'Paying Back DAI');
     reset();
   };
 
+  const { allowance, userStartedWithoutAllowance, loading } = allowanceState;
   const amt = parseFloat(amount);
   const isLessThanBalance = amt <= daiBalance;
   const isLessThanDebt = amt <= cdp.debt.toNumber();
   const isNonZero = amount !== '' && amt > 0;
-  const valid = isNonZero && isLessThanDebt && isLessThanBalance;
+  const canPayBack =
+    isNonZero && isLessThanDebt && isLessThanBalance && allowance;
 
   let errorMessage = null;
   if (!isLessThanBalance && isNonZero)
@@ -92,8 +150,33 @@ const Payback = ({ cdp, reset }) => {
             }
           />
         </Grid>
+        {userStartedWithoutAllowance && (
+          <Grid gridTemplateColumns="2fr 1fr" gridColumnGap="s">
+            {!allowance && !loading && (
+              <Text.p t="body">{lang.action_sidebar.unlock_dai}</Text.p>
+            )}
+            {!allowance && loading && (
+              <Text.p t="body">{lang.action_sidebar.unlocking_dai}</Text.p>
+            )}
+            {allowance && !loading && (
+              <Text.p t="body">{lang.action_sidebar.dai_unlocked}</Text.p>
+            )}
+
+            {loading ? (
+              <Box alignSelf="center" justifySelf="end">
+                <Loader size="20" />
+              </Box>
+            ) : (
+              <Toggle
+                active={allowance}
+                onClick={setAllowance}
+                justifySelf="end"
+              />
+            )}
+          </Grid>
+        )}
         <Grid gridTemplateColumns="1fr 1fr" gridColumnGap="s">
-          <Button disabled={!valid} onClick={payback}>
+          <Button disabled={!canPayBack} onClick={payback}>
             {lang.actions.pay_back}
           </Button>
           <Button variant="secondary-outline" onClick={reset}>
