96c53d2074fc4904c99fa6861f2d445ab07ca16d
sirromdev <sirromdev@gmail.com>
2019-06-06 16:13:13 +0100

    test commit

--- src/components/SiteVersion.js
+++ src/components/SiteVersion.js
@@ -1,27 +1,8 @@
 import React from 'react';
-import { Link } from '@makerdao/ui-components-core';
-import styled from 'styled-components';
-import { message } from '../static/version.json';
-
-const VersionLink = styled(Link)`
-  font-size: 0.75em;
-  color: lightgray;
-  &:hover {
-    color: gray;
-  }
-`;
 
 const SiteVersion = () => {
-  const commitId = message.substr(0, 7);
-
-  return (
-    <VersionLink
-      href={`https://github.com/makerdao/mcd-cdp-portal/tree/${commitId}`}
-      target="_blank"
-    >
-      {`Site version: ${message}`}
-    </VersionLink>
-  );
+  console.log(process.env);
+  return <div>ugfiuy</div>;
 };
 
 export default SiteVersion;
