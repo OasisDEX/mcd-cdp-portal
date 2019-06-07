7d2b537c8dda528112d54f5b2b3b526b0b4b1da8
sirromdev <sirromdev@gmail.com>
2019-06-07 14:39:04 +0100

    added link for url to commit

--- src/components/SiteVersion.js
+++ src/components/SiteVersion.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { Link } from '@makerdao/ui-components-core';
 
 const GithubInfo = () => {
   const {
     NOW_GITHUB_REPO,
     NOW_GITHUB_ORG,
     NOW_GITHUB_COMMIT_SHA,
-    NOW_GITHUB_COMMIT_REPO,
     NOW_GITHUB_COMMIT_AUTHOR_NAME
   } = require('../static/version.json');
 
@@ -13,10 +13,10 @@ const GithubInfo = () => {
     NOW_GITHUB_REPO,
     NOW_GITHUB_ORG,
     NOW_GITHUB_COMMIT_SHA,
-    NOW_GITHUB_COMMIT_REPO,
     NOW_GITHUB_COMMIT_AUTHOR_NAME
   );
-  return <div>production</div>;
+  const commitUrl = `https://github.com/${NOW_GITHUB_ORG}/${NOW_GITHUB_REPO}/commit/${NOW_GITHUB_COMMIT_SHA}`;
+  return <Link href={commitUrl}>production</Link>;
 };
 
 const SiteVersion = () => {
