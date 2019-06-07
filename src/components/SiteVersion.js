c6722fd31de07b4fd913a73d52c60582f7886958
sirromdev <sirromdev@gmail.com>
2019-06-07 14:19:54 +0100

    included more content

--- src/components/SiteVersion.js
+++ src/components/SiteVersion.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { NOW_GITHUB_REPO } from '../static/version.json';
+
+const GithubInfo = () => {
+  const {
+    NOW_GITHUB_REPO,
+    NOW_GITHUB_ORG,
+    NOW_GITHUB_COMMIT_SHA,
+    NOW_GITHUB_COMMIT_REPO,
+    NOW_GITHUB_COMMIT_AUTHOR_NAME
+  } = require('../static/version.json');
+
+  console.log(
+    NOW_GITHUB_REPO,
+    NOW_GITHUB_ORG,
+    NOW_GITHUB_COMMIT_SHA,
+    NOW_GITHUB_COMMIT_REPO,
+    NOW_GITHUB_COMMIT_AUTHOR_NAME
+  );
+  return <div>production</div>;
+};
 
 const SiteVersion = () => {
-  console.log(NOW_GITHUB_REPO);
   return process.env.NODE_ENV === 'production' ? (
-    <div>production</div>
+    <GithubInfo />
   ) : (
     <div>local</div>
   );
