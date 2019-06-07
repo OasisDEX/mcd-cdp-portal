44fe45e2088a4a21e71e58f5f5f172e82c6dd5cd
sirromdev <sirromdev@gmail.com>
2019-06-07 16:25:19 +0100

    rendering GH information

--- src/components/SiteVersion.js
+++ src/components/SiteVersion.js
@@ -1,29 +1,77 @@
-import React from 'react';
-import { Link } from '@makerdao/ui-components-core';
+import React, { useState, useEffect } from 'react';
+import { Grid, Box, Link, Text } from '@makerdao/ui-components-core';
+import 'whatwg-fetch';
+import styled from 'styled-components';
 
-const GithubInfo = () => {
-  const {
-    NOW_GITHUB_REPO,
-    NOW_GITHUB_ORG,
-    NOW_GITHUB_COMMIT_SHA,
-    NOW_GITHUB_COMMIT_AUTHOR_NAME
-  } = require('../static/version.json');
+const VersionText = styled(Text)`
+  font-size: 0.75em;
+  color: lightgray;
+`;
+const VersionLink = styled(Link)`
+  &:hover {
+    ${VersionText} {
+      color: gray;
+    }
+    ${Grid} {
+      border: 1px solid gray;
+      border-radius: 3px;
+    }
+  }
+`;
+const {
+  NOW_GITHUB_REPO,
+  NOW_GITHUB_ORG,
+  NOW_GITHUB_COMMIT_SHA,
+  NOW_GITHUB_COMMIT_AUTHOR_NAME,
+  NOW_GITHUB_COMMIT_REF
+} = require('../static/version.json');
+
+const SiteVersion = () => {
+  const [commitMsg, setCommitMsg] = useState('');
+  const [commitDate, setCommitDate] = useState('');
+  const apiUrl = `https://api.github.com/repos/${NOW_GITHUB_ORG}/${NOW_GITHUB_REPO}/git/commits/${NOW_GITHUB_COMMIT_SHA}`;
+
+  useEffect(() => {
+    const fetchCommitMsg = async () => {
+      const rawJson = await fetch(apiUrl);
+      const commitObj = await rawJson.json();
+      const {
+        message,
+        committer: { date }
+      } = commitObj;
+      const d = new Date(date);
+      setCommitDate(d.toUTCString());
+      setCommitMsg(message);
+    };
+    fetchCommitMsg();
+  }, [apiUrl]);
 
   console.log(
     NOW_GITHUB_REPO,
     NOW_GITHUB_ORG,
     NOW_GITHUB_COMMIT_SHA,
-    NOW_GITHUB_COMMIT_AUTHOR_NAME
+    NOW_GITHUB_COMMIT_AUTHOR_NAME,
+    NOW_GITHUB_COMMIT_REF
   );
   const commitUrl = `https://github.com/${NOW_GITHUB_ORG}/${NOW_GITHUB_REPO}/commit/${NOW_GITHUB_COMMIT_SHA}`;
-  return <Link href={commitUrl}>production</Link>;
-};
+  return (
+    <Box>
+      {commitMsg !== '' ? (
+        <VersionLink href={commitUrl} target="_blank">
+          <Grid p="s" gridTemplateColumns="1fr 2fr">
+            <VersionText>Author:</VersionText>
+            <VersionText>{NOW_GITHUB_COMMIT_AUTHOR_NAME}</VersionText>
 
-const SiteVersion = () => {
-  return process.env.NODE_ENV === 'production' ? (
-    <GithubInfo />
-  ) : (
-    <div>local</div>
+            <VersionText>Commit:</VersionText>
+            <VersionText>{commitMsg}</VersionText>
+            <VersionText>Hash:</VersionText>
+            <VersionText>{NOW_GITHUB_COMMIT_SHA.substring(0, 10)}</VersionText>
+            <VersionText>Uploaded:</VersionText>
+            <VersionText>{commitDate}</VersionText>
+          </Grid>
+        </VersionLink>
+      ) : null}
+    </Box>
   );
 };
 
