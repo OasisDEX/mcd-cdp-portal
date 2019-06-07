96ee64b64044affff59f5dd9040b706b7c603fae
Pádraic Ó Mhúiris <sirromdev@gmail.com>
2019-06-07 22:03:38 +0100

    CDP-324 Display up-to-date commit hashes
    * CDP-324 Display up-to-date commit hashes

--- src/components/SiteVersion.js
+++ src/components/SiteVersion.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import { Link } from '@makerdao/ui-components-core';
+import React, { useState, useEffect } from 'react';
+import { Grid, Box, Link, Text } from '@makerdao/ui-components-core';
+import 'whatwg-fetch';
 import styled from 'styled-components';
-import { message } from '../static/version.json';
 
-const VersionLink = styled(Link)`
+const VersionText = styled(Text)`
   font-size: 0.75em;
   color: lightgray;
+`;
+const VersionLink = styled(Link)`
   &:hover {
-    color: gray;
+    ${VersionText} {
+      color: gray;
+    }
+    ${Grid} {
+      border: 1px solid gray;
+      border-radius: 3px;
+    }
   }
 `;
 
 const SiteVersion = () => {
-  const commitId = message.substr(0, 7);
+  const {
+    NOW_GITHUB_REPO,
+    NOW_GITHUB_ORG,
+    NOW_GITHUB_COMMIT_SHA,
+    NOW_GITHUB_COMMIT_AUTHOR_NAME,
+    NOW_GITHUB_COMMIT_REF
+  } = require('../static/version.json');
 
+  const [commitMsg, setCommitMsg] = useState('');
+  const [commitDate, setCommitDate] = useState('');
+  const apiUrl = `https://api.github.com/repos/${NOW_GITHUB_ORG}/${NOW_GITHUB_REPO}/git/commits/${NOW_GITHUB_COMMIT_SHA}`;
+
+  useEffect(() => {
+    const fetchCommitMsg = async () => {
+      const rawJson = await fetch(apiUrl);
+      const commitObj = await rawJson.json();
+      const {
+        message,
+        committer: { date }
+      } = commitObj;
+      const d = new Date(date);
+      setCommitDate(d.toLocaleString());
+      setCommitMsg(message);
+    };
+    fetchCommitMsg();
+  }, [apiUrl]);
+
+  const commitUrl = `https://github.com/${NOW_GITHUB_ORG}/${NOW_GITHUB_REPO}/commit/${NOW_GITHUB_COMMIT_SHA}`;
   return (
-    <VersionLink
-      href={`https://github.com/makerdao/mcd-cdp-portal/tree/${commitId}`}
-      target="_blank"
-    >
-      {`Site version: ${message}`}
-    </VersionLink>
+    <Box>
+      {commitMsg !== '' ? (
+        <VersionLink href={commitUrl} target="_blank">
+          <Grid p="s" gridTemplateColumns="1fr 2fr">
+            <VersionText>Author:</VersionText>
+            <VersionText>{NOW_GITHUB_COMMIT_AUTHOR_NAME}</VersionText>
+
+            <VersionText>Commit:</VersionText>
+            <VersionText>{commitMsg}</VersionText>
+
+            <VersionText>Hash:</VersionText>
+            <VersionText>{NOW_GITHUB_COMMIT_SHA.substring(0, 10)}</VersionText>
+
+            <VersionText>Branch:</VersionText>
+            <VersionText>{NOW_GITHUB_COMMIT_REF}</VersionText>
+
+            <VersionText>Uploaded:</VersionText>
+            <VersionText>{commitDate}</VersionText>
+          </Grid>
+        </VersionLink>
+      ) : null}
+    </Box>
   );
 };
 
---
+++ src/scripts/write-env-vars.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+NOW_VARS=$(printenv | grep '^NOW')
+NOW_VARS_ARR=(`echo ${NOW_VARS}`);
+JSON_STRING='{'
+for var in "${NOW_VARS_ARR[@]}"
+do
+    var_str=$(echo $var | tr '=' '\n')
+    var_arr=(`echo ${var_str}`)
+    key=${var_arr[0]}
+    val=${var_arr[1]}
+    JSON_STRING+="\n  \"${key}\": \"${val}\","
+done
+echo -e "${JSON_STRING::-1}\n}" > ./src/static/version.json
