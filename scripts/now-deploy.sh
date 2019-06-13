385494b5df1d9932b250fff1f956135db023b1a0
Lawrence Wang <l@lw.io>
2019-06-12 20:27:33 -0700

    no need for tee
    the now cli is smart--its stdout is just the one piece of info i need

--- scripts/now-deploy.sh
+++ scripts/now-deploy.sh
@@ -4,7 +4,6 @@ set -e
 ./scripts/write-env-vars.sh
 cat ./src/static/version.json
 
-now --token $NOW_TOKEN --scope mkr-js-prod | tee now-deploy.log
-FROM_NAME=`cat now-deploy.log | grep "Aliased to" | awk '{print $5}'`
+FROM_NAME=`now --token $NOW_TOKEN --scope mkr-js-prod`
 TO_NAME=mcd-cdp-portal-git-${TRAVIS_BRANCH}.mkr-js-prod.now.sh
 now alias --token $NOW_TOKEN --scope mkr-js-prod $FROM_NAME $TO_NAME
