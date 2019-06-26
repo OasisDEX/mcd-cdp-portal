3b2717bb7567368be21316edc2669f15f1c92207
Lawrence Wang <l@lw.io>
2019-06-26 16:50:46 -0700

    allow setting initialState on StoreProvider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Suspense, useEffect } from 'react';
 import { StateInspector } from 'reinspect';
-import { StoreProvider } from './providers/StoreProvider';
+import StoreProvider from './providers/StoreProvider';
 import styled, { ThemeProvider } from 'styled-components';
 import { Router, NotFoundBoundary } from 'react-navi';
 import { createBrowserNavigation } from 'navi';
