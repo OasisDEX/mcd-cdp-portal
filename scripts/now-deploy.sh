3145f824f567abf1479c3157e6f50f2957ffaa4c
Lawrence Wang <l@lw.io>
2019-07-02 14:19:02 -0700

    update now-deploy.sh to handle tag/branch names with dots

--- scripts/now-deploy.sh
+++ scripts/now-deploy.sh
@@ -5,5 +5,5 @@ set -e
 cat ./src/static/version.json
 
 FROM_NAME=`now --token $NOW_TOKEN --scope mkr-js-prod`
-TO_NAME=mcd-cdp-portal-git-${TRAVIS_BRANCH}.mkr-js-prod.now.sh
+TO_NAME=mcd-cdp-portal-git-${TRAVIS_BRANCH//./-}.mkr-js-prod.now.sh
 now alias --token $NOW_TOKEN --scope mkr-js-prod $FROM_NAME $TO_NAME
