45771ba75a56eb4ae4f4fbb6fb78cf4032c016aa
Lawrence Wang <l@lw.io>
2019-07-16 18:29:11 -0700

    CDP-350 add body-scroll-lock to FullScreenAction

--- src/components/CDPDisplay/FullScreenAction.js
+++ src/components/CDPDisplay/FullScreenAction.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import sidebars from '../Sidebars';
 import { Box } from '@makerdao/ui-components-core';
 import SidebarActionLayout from '../../layouts/SidebarActionLayout';
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 
 export default function FullScreenAction({ type, reset, props }) {
   const domNode = document.getElementById('portal1');
   const SidebarComponent = sidebars[type];
+  const ref = useRef();
+
+  useEffect(() => {
+    const scrollable = ref.current;
+    disableBodyScroll(scrollable);
+    return () => enableBodyScroll(scrollable);
+  }, []);
+
   return ReactDOM.createPortal(
     <Box
+      ref={ref}
       css={`
         top: 0;
         left: 0;
