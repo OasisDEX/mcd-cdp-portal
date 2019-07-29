d2e7e530b4285969107aa574a5c5d2c9ce567935
Lawrence Wang <l@lw.io>
2019-07-29 11:47:06 -0700

    lowercase branch name in now alias

--- scripts/now-deploy.sh
+++ scripts/now-deploy.sh
@@ -5,5 +5,6 @@ set -e
 cat ./src/static/version.json
 
 FROM_NAME=`now --token $NOW_TOKEN --scope mkr-js-prod`
-TO_NAME=mcd-cdp-portal-git-${TRAVIS_BRANCH//./-}.mkr-js-prod.now.sh
+BRANCH=`echo ${TRAVIS_BRANCH//./-} | tr '[:upper:]' '[:lower:]'`
+TO_NAME=mcd-cdp-portal-git-${BRANCH}.mkr-js-prod.now.sh
 now alias --token $NOW_TOKEN --scope mkr-js-prod $FROM_NAME $TO_NAME
