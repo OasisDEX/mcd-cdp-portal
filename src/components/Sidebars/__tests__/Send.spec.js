ec4763825589c33ec0893065c9cc7d3e8f8050b8
Ethan <ebennett19@gmail.com>
2019-09-30 23:30:59 -0600

    Fix send.spec

--- src/components/Sidebars/__tests__/Send.spec.js
+++ src/components/Sidebars/__tests__/Send.spec.js
@@ -9,15 +9,60 @@ import '@testing-library/jest-dom/extend-expect';
 import { renderForSidebar as render } from '../../../../test/helpers/render';
 import Send from '../Send';
 import lang from 'languages';
-import BigNumber from 'bignumber.js';
 import { mineBlocks } from '@makerdao/test-helpers';
-import { TestAccountProvider } from '../../../../node_modules/@makerdao/test-helpers/dist/TestAccountProvider';
 import testAccounts from '../../../../node_modules/@makerdao/test-helpers/dist/testAccounts.json';
 import useMaker from '../../../hooks/useMaker';
 import useWalletBalances from '../../../hooks/useWalletBalances';
 import { calculateGasCost } from '../../../utils/ethereum';
 
 afterEach(cleanup);
+
+test('should send 1 ETH successfully', async () => {
+  const { getByTestId, getAllByTestId } = render(<SetupSend token="ETH" />);
+  const {
+    addresses: [addr1, addr2]
+  } = testAccounts;
+
+  const [amountElements, addressElements, sendButton] = await Promise.all([
+    waitForElement(() => getAllByTestId('send-amount-input')),
+    waitForElement(() => getAllByTestId('send-address-input')),
+    waitForElement(() => getByTestId('send-button'))
+  ]);
+
+  const [beforeBal1, beforeBal2] = await Promise.all([
+    getTokenBalance(addr1, 'ETH'),
+    getTokenBalance(addr2, 'ETH')
+  ]);
+
+  const amountInput = amountElements[2];
+  const addressInput = addressElements[2];
+  act(() => {
+    fireEvent.change(amountInput, { target: { value: '1' } });
+    fireEvent.change(addressInput, { target: { value: addr2 } });
+  });
+
+  expect(amountInput.value).toBe('1');
+  expect(addressInput.value).toBe(addr2);
+
+  act(() => {
+    fireEvent.click(sendButton);
+  });
+
+  await mineBlocks(_maker.service('web3'));
+
+  const [afterBal1, afterBal2] = await Promise.all([
+    getTokenBalance(addr1, 'ETH'),
+    getTokenBalance(addr2, 'ETH')
+  ]);
+
+  expect(
+    afterBal1
+      .minus(beforeBal1)
+      .plus(_gasCost)
+      .toString()
+  ).toEqual('-1');
+  expect(afterBal2.minus(beforeBal2).toString()).toEqual('1');
+});
 
 test('basic rendering when sending ETH', async () => {
   const token = 'ETH';
@@ -85,50 +130,3 @@ const SetupSend = ({ token }) => {
     <>{hasTokenBalance ? <Send token={token} reset={() => null} /> : <div />}</>
   );
 };
-
-test('should send 1 ETH successfully', async () => {
-  const { getByTestId, getAllByTestId } = render(<SetupSend token="ETH" />);
-  const {
-    addresses: [addr1, addr2]
-  } = testAccounts;
-
-  const [amountElements, addressElements, sendButton] = await Promise.all([
-    waitForElement(() => getAllByTestId('send-amount-input')),
-    waitForElement(() => getAllByTestId('send-address-input')),
-    waitForElement(() => getByTestId('send-button'))
-  ]);
-
-  const [beforeBal1, beforeBal2] = await Promise.all([
-    getTokenBalance(addr1, 'ETH'),
-    getTokenBalance(addr2, 'ETH')
-  ]);
-
-  const amountInput = amountElements[2];
-  const addressInput = addressElements[2];
-  act(() => {
-    fireEvent.change(amountInput, { target: { value: '1' } });
-    fireEvent.change(addressInput, { target: { value: addr2 } });
-  });
-
-  expect(amountInput.value).toBe('1');
-  expect(addressInput.value).toBe(addr2);
-
-  act(() => {
-    fireEvent.click(sendButton);
-  });
-
-  await mineBlocks(_maker.service('web3'));
-
-  const [afterBal1, afterBal2] = await Promise.all([
-    getTokenBalance(addr1, 'ETH'),
-    getTokenBalance(addr2, 'ETH')
-  ]);
-
-  expect(
-    afterBal1
-      .minus(beforeBal1)
-      .plus(_gasCost)
-      .toString()
-  ).toEqual('-1');
-  expect(afterBal2.minus(beforeBal2).toString()).toEqual('1');
-});
