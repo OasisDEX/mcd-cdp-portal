1dae56234e7133ee3c0890bcf9296c5de9fc2230
sirromdev <sirromdev@gmail.com>
2019-10-25 16:03:29 +1100

    CDP-444 Reverted keys liquidation_fee -> liquadtion_penalty

--- src/components/CDPCreateScreens/CDPCreateSelectCollateral.js
+++ src/components/CDPCreateScreens/CDPCreateSelectCollateral.js
@@ -31,7 +31,10 @@ const CDPCreateSelectCollateralSidebar = () => {
             lang.liquidation_ratio,
             lang.cdp_create.liquidation_ratio_description
           ],
-          [lang.liquidation_fee, lang.cdp_create.liquidation_fee_description]
+          [
+            lang.liquidation_penalty,
+            lang.cdp_create.liquidation_penalty_description
+          ]
         ].map(([title, text]) => (
           <Grid mb="m" key={title} gridRowGap="xs">
             <TextBlock t="h5" lineHeight="normal">
