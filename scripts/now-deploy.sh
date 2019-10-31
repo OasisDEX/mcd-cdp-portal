8a6b5716f5f415d6f23bbe9d7d6d56d99b14814f
Lawrence Wang <l@lw.io>
2019-10-31 13:15:39 -0700

    deploy to now from circleci

--- scripts/now-deploy.sh
+++ scripts/now-deploy.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
-./scripts/write-env-vars.sh
+cat > ./src/static/version.json <<- EOM
+{
+  "COMMIT_SHA": "${CIRCLE_SHA1}",
+  "COMMIT_BRANCH": "${CIRCLE_BRANCH}"
+}
+EOM
+
 cat ./src/static/version.json
 
 FROM_NAME=`now --token $NOW_TOKEN --scope mkr-js-prod`
-BRANCH=`echo ${TRAVIS_BRANCH//./-} | tr '[:upper:]' '[:lower:]'`
+BRANCH=`echo ${CIRCLE_BRANCH//./-} | tr '[:upper:]' '[:lower:]'`
 TO_NAME=mcd-cdp-portal-git-${BRANCH}.mkr-js-prod.now.sh
 now alias --token $NOW_TOKEN --scope mkr-js-prod $FROM_NAME $TO_NAME
