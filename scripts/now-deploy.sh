b215c7a69563f7a07b6114fd69693a48b095de90
Lawrence Wang <l@lw.io>
2019-10-31 13:34:51 -0700

    fix path to now binary

--- scripts/now-deploy.sh
+++ scripts/now-deploy.sh
@@ -13,4 +13,8 @@ cat ./src/static/version.json
 FROM_NAME=`now --token $NOW_TOKEN --scope mkr-js-prod`
 BRANCH=`echo ${CIRCLE_BRANCH//./-} | tr '[:upper:]' '[:lower:]'`
 TO_NAME=mcd-cdp-portal-git-${BRANCH}.mkr-js-prod.now.sh
-now alias --token $NOW_TOKEN --scope mkr-js-prod $FROM_NAME $TO_NAME
+
+./node_modules/.bin/now alias \
+  --token $NOW_TOKEN \
+  --scope mkr-js-prod \
+  $FROM_NAME $TO_NAME
