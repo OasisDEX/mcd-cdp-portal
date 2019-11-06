74718ffa01e5a5cc8fbb3ab14dae4138f0972b7e
Adrian le Bas <adrianleb@gmail.com>
2019-11-06 16:54:35 +0100

    Implement walletconnect plugin and clean mixpanel.identify calls

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -18,7 +18,8 @@ export const AccountTypes = {
   LEDGER: 'ledger',
   TREZOR: 'trezor',
   METAMASK: 'browser',
-  WALLETLINK: 'walletlink'
+  WALLETLINK: 'walletlink',
+  WALLETCONNECT: 'walletconnect'
 };
 
 export const Routes = {
