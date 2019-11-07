1eeb98948cf80386277f5f75c202eae8facd4198
sirromdev <sirromdev@gmail.com>
2019-11-07 17:23:36 +1100

    Show display feeds toggle when > 3 feeds

--- src/components/SidebarFeeds.js
+++ src/components/SidebarFeeds.js
@@ -50,21 +50,23 @@ const SidebarFeeds = ({ feeds }) => {
             )
         )}
       </CardBody>
-      <StyledCardBody p="s" onClick={() => setCollapsed(!collapsed)}>
-        <Flex justifyContent="center" alignItems="center">
-          {collapsed ? (
-            <>
-              <Text pr="xs">{lang.sidebar.view_more}</Text>
-              <Carat />
-            </>
-          ) : (
-            <>
-              <Text pr="xs">{lang.sidebar.view_less}</Text>
-              <Carat rotation={180} />
-            </>
-          )}
-        </Flex>
-      </StyledCardBody>
+      {feeds.length > 3 && (
+        <StyledCardBody p="s" onClick={() => setCollapsed(!collapsed)}>
+          <Flex justifyContent="center" alignItems="center">
+            {collapsed ? (
+              <>
+                <Text pr="xs">{lang.sidebar.view_more}</Text>
+                <Carat />
+              </>
+            ) : (
+              <>
+                <Text pr="xs">{lang.sidebar.view_less}</Text>
+                <Carat rotation={180} />
+              </>
+            )}
+          </Flex>
+        </StyledCardBody>
+      )}
     </Card>
   );
 };
