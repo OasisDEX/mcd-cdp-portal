f5224f9a49d451793eb8b7781ebe01ad5d583034
Lawrence Wang <l@lw.io>
2019-11-08 13:54:41 -0800

    when history flag is disabled, don't fetch data

--- src/components/CDPDisplay/Presentation.js
+++ src/components/CDPDisplay/Presentation.js
@@ -70,9 +70,12 @@ export default function({ cdp, showSidebar, account, network }) {
 
   const [eventHistory, setEventHistory] = useState([]);
 
-  useEffect(() => {
-    getEventHistory(maker, cdpId).then(events => setEventHistory(events));
-  }, [maker, cdpId]);
+  if (FF_VAULTHISTORY) {
+    useEffect(() => {
+      getEventHistory(maker, cdpId).then(events => setEventHistory(events));
+    }, [maker, cdpId]);
+  }
+  
   return (
     <PageContentLayout>
       <Box>
