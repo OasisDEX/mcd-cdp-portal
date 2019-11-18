02d47161386d13326ad9be3dec89c1711fe9dda3
sirromdev <sirromdev@gmail.com>
2019-11-18 12:18:14 +0000

    Fix waiting page mobile view

--- src/pages/Waiting.js
+++ src/pages/Waiting.js
@@ -3,21 +3,17 @@ import { Flex, Text } from '@makerdao/ui-components-core';
 
 const Waiting = () => (
   <Flex
-    maxWidth="100%"
     width="100vw"
     height="100vh"
     alignItems="center"
     justifyContent="center"
+    flexDirection="column"
   >
-    <div>
-      <Text.h2 textAlign="center">Coming soon</Text.h2>
-      <Flex py="m" justifyContent="center">
-        <Text>
-          You will be able to create Maker Vaults and secure your Dai into Oasis
-          Save from 16:00 UTC today
-        </Text>
-      </Flex>
-    </div>
+    <Text.h2 textAlign="center">Coming soon</Text.h2>
+    <Text py="m" mx="m">
+      You will be able to create Maker Vaults and secure your Dai into Oasis
+      Save from 16:00 UTC today
+    </Text>
   </Flex>
 );
 
