3713585d374b094584f95bd5023db6e155307238
sirromdev <sirromdev@gmail.com>
2019-11-28 13:19:47 +0000

    your -> you are

--- src/languages/_english.js
+++ src/languages/_english.js
@@ -175,7 +175,7 @@ export default {
     'post_confirmed_text': 'You can safely leave this page.',
     'insufficient_ilk_balance': 'Insufficient {0} balance',
     'has_understood_stability_fee': 'I understand the Stability Fee is not fixed and is likely to change over time',
-    'collateralization_warning': 'The amount of Dai your generating is putting your Vault at risk of liquidation',
+    'collateralization_warning': 'The amount of Dai you are generating is putting your Vault at risk of liquidation',
     'draw_too_much_dai': 'Vault below liquidation threshold',
     'below_dust_limit': 'A Vault requires a minimum of {0} Dai to be generated',
     'dust_max_payback': 'You can repay all your outstanding debt, or a maximum of {0} Dai',
