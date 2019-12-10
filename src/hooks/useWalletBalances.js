107bd257876020aab42a550a503faf7bd4e36f08
sirromdev <sirromdev@gmail.com>
2019-12-10 15:03:45 +0000

    Corrected miscalculation for wallet balances DSR amount

--- src/hooks/useWalletBalances.js
+++ src/hooks/useWalletBalances.js
@@ -1,15 +1,16 @@
 import useStore from 'hooks/useStore';
 import useMaker from 'hooks/useMaker';
+import { getSavingsBalance } from 'reducers/accounts';
 
 const useWalletBalances = () => {
-  const [{ accounts }] = useStore();
+  const [{ accounts, savings }] = useStore();
   const { account } = useMaker();
 
   const walletConnected = account && accounts && accounts[account.address];
   return walletConnected
     ? {
         ...accounts[account.address].balances,
-        DSR: accounts[account.address].savings
+        DSR: getSavingsBalance(account.address, { accounts, savings })
       }
     : {};
 };
