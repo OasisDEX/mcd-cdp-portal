1e2638f4f1f03aeecafee8ec9bc9d4a17f8830a6
sirromdev <sirromdev@gmail.com>
2019-12-12 13:58:32 +0000

    Corrected cursor in SetMax

--- src/components/SetMax.js
+++ src/components/SetMax.js
@@ -6,7 +6,7 @@ import useLanguage from 'hooks/useLanguage';
 export default function SetMax({ ...props }) {
   const { lang } = useLanguage();
   return (
-    <Text fontWeight="medium" color="blue" {...props}>
+    <Text fontWeight="medium" color="blue" cursor="pointer" {...props}>
       {lang.set_max}
     </Text>
   );
