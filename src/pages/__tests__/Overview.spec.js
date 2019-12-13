3dcd1c6fe83d770ffbbd2d78238bf24bbfa16c87
Phil Bain <bain.pmcg@gmail.com>
2019-12-13 18:16:06 +0100

    CDP-551 test for overview page

---
+++ src/pages/__tests__/Overview.spec.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import * as navi from 'react-navi';
+import { waitForElement, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { MDAI, ETH } from '@makerdao/dai-plugin-mcd';
+import useMaker from '../../hooks/useMaker';
+
+import Overview from '../Overview';
+import { renderWithMaker } from '../../../test/helpers/render';
+import { instantiateMaker } from '../../maker';
+import styled from 'styled-components';
+
+jest.mock('react-navi');
+
+const ILK = 'ETH-A';
+const VAULT1_ETH = '6';
+const VAULT1_ART = '80.1234567';
+const VAULT2_ETH = '1';
+const VAULT2_ART = '25';
+const VIEWED_ADDRESS = '0x16fb96a5fa0427af0c8f7cf1eb4870231c8154b6';
+
+let maker;
+let account;
+
+beforeAll(async () => {
+  maker = await instantiateMaker({ network: 'testnet' });
+  await await maker
+    .service('mcd:cdpManager')
+    .openLockAndDraw(ILK, ETH(VAULT1_ETH), MDAI(VAULT1_ART));
+
+  await await maker
+    .service('mcd:cdpManager')
+    .openLockAndDraw(ILK, ETH(VAULT2_ETH), MDAI(VAULT2_ART));
+});
+
+function WaitForAccount({ children, callback }) {
+  const { account } = useMaker();
+  callback(account);
+  return account ? children : null;
+}
+
+function prepState(state) {
+  return {
+    ...state,
+    cdps: {
+      1: {
+        ilk: ILK,
+        ink: VAULT1_ETH,
+        art: VAULT1_ART
+      },
+      2: {
+        ilk: ILK,
+        ink: VAULT2_ETH,
+        art: VAULT2_ART
+      }
+    }
+  };
+}
+
+afterEach(cleanup);
+
+test('render overview page and display calculated vault values', async () => {
+  navi.useCurrentRoute.mockReturnValue({ url: { search: '?network=testnet' } });
+  navi.Link = styled.a``;
+  const { getByText, getAllByText } = renderWithMaker(
+    <WaitForAccount
+      callback={a => {
+        account = a;
+      }}
+    >
+      <Overview viewedAddress={VIEWED_ADDRESS} />
+    </WaitForAccount>,
+    prepState,
+    null,
+    { viewedAddress: VIEWED_ADDRESS }
+  );
+
+  // Wait for page to render
+  await waitForElement(() => account);
+  await waitForElement(() => getByText('Overview'));
+
+  // Total collateral locked
+  getByText('$1050.00 USD');
+  // Total Dai debt
+  getByText('105.26 DAI');
+  // Vault1 Dai debt
+  getByText('80.23 DAI');
+  // Current ratio
+  getByText('1121.77%');
+  // Deposited
+  getByText('6.00 ETH');
+  // Available to withdraw
+  getByText('5.20 ETH');
+  // Privacy policy
+  getByText('privacy policy');
+
+  // Manage vault buttons link to correct vault
+  const [vault2, vault1] = getAllByText('Manage Vault');
+  expect(vault1.closest('a')).toHaveAttribute(
+    'href',
+    '/borrow/1?network=testnet'
+  );
+  expect(vault2.closest('a')).toHaveAttribute(
+    'href',
+    '/borrow/2?network=testnet'
+  );
+}, 15000);
