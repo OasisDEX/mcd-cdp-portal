2d45a3b6fc8a72f092ddd048971c87368e407285
Michael Elliot <michael@digital.com.au>
2019-12-16 17:31:58 +0800

    Use websocket provider if ws query param is set

--- src/maker.js
+++ src/maker.js
@@ -7,6 +7,7 @@ import walletConnectPlugin from '@makerdao/dai-plugin-walletconnect';
 import configPlugin from '@makerdao/dai-plugin-config';
 import networkConfig from './references/config';
 import { networkNameToId } from './utils/network';
+import { getQueryParamByName } from './utils/dev';
 
 let _maker;
 
@@ -42,7 +43,7 @@ export async function instantiateMaker({
     },
     provider: {
       url: rpcUrl,
-      type: 'HTTP'
+      type: getQueryParamByName('ws') ? 'WEBSOCKET' : 'HTTP'
     },
     web3: {
       pollingInterval: network === 'testnet' ? 100 : null
--- src/utils/dev.js
+++ src/utils/dev.js
@@ -11,3 +11,12 @@ export const uniqueId = (() => {
     return map.get(object);
   };
 })();
+
+export const getQueryParamByName = (name, url) => {
+  if (!url) url = window.location.href;
+  const regex = new RegExp(`[?&]${name}(=([^&#]*)|&|#|$)`);
+  const results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+};
