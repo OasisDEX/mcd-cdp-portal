3f8bcfe228d994cb488e84eff11659cd15a72f1d
sirromdev <sirromdev@gmail.com>
2020-02-21 16:37:24 +0000

    removed cdps reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,17 @@
 import { enableBatching } from 'utils/redux';
 import systemReducer from 'reducers/system';
 import feedsReducer from 'reducers/feeds';
-import cdpsReducer from 'reducers/cdps';
 import savingsReducer from 'reducers/savings';
 import notificationsReducer from 'reducers/notifications';
 import mathReducer from './math';
 
 const rootReducer = (
-  { system, feeds, cdps, accounts, savings, notifications, raw },
+  { system, feeds, savings, notifications, raw },
   action
 ) => {
   const combinedState = {
     system: systemReducer(system, action),
     feeds: feedsReducer(feeds, action),
-    cdps: cdpsReducer(cdps, action),
     savings: savingsReducer(savings, action),
     notifications: notificationsReducer(notifications, action),
     raw
