a9968bfc918fd3ce7a4a645484bcb81cec0812ff
Fernando Tiberti <fertiberti@gmail.com>
2020-03-31 20:45:05 -0300

    Organize page sections' code.

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -30,170 +30,290 @@ const Content = ({ children }) => (
   </Box>
 );
 
-const Cards = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  margin: 80px auto;
-  padding-bottom: 66px;
+const Cards = (() => {
+  const CardsContainer = styled(Box)`
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin-right: auto;
+    margin-left: auto;
+    padding-bottom: 66px;
 
-  :after {
-    content: ' ';
-    display: block;
-    position: absolute;
-    z-index: -1;
-    bottom: 0;
-    width: 93%;
-    left: 3.5%;
-    height: 95%;
-    background: linear-gradient(
-      180deg,
-      rgba(255, 249, 237, 0) 0%,
-      #fff9ed 100%
-    );
-  }
-
-  @media (max-width: 1238px) {
-    max-width: 368px;
     :after {
-      content: none;
+      content: ' ';
+      display: block;
+      position: absolute;
+      z-index: -1;
+      bottom: 0;
+      width: 93%;
+      left: 3.5%;
+      height: 95%;
+      background: linear-gradient(
+        180deg,
+        rgba(255, 249, 237, 0) 0%,
+        #fff9ed 100%
+      );
     }
-  }
-`;
 
-const Card = styled.div`
-  overflow: hidden;
-  width: 368px;
-  height: 430px;
-  position: relative;
-  flex-shrink: 1;
-  text-align: left;
-  padding: 57px 40px 60px;
+    @media (max-width: 1238px) {
+      max-width: 368px;
+      :after {
+        content: none;
+      }
+    }
+  `;
 
-  @media (max-width: 1238px) {
-    margin-bottom: 35px;
-  }
+  const Card = styled.div`
+    overflow: hidden;
+    width: 368px;
+    height: 430px;
+    position: relative;
+    flex-shrink: 1;
+    text-align: left;
+    padding: 57px 40px 60px;
 
-  .title {
-    font-size: 28px;
-    line-height: 28px;
-    margin-top: 21px;
-    margin-bottom: 13px;
-    font-weight: bold;
-    color: ${getColor('darkPurple')};
-  }
+    @media (max-width: 1238px) {
+      margin-bottom: 35px;
+    }
 
-  .buttonContainer {
-    position: absolute;
-    bottom: 59px;
-    width: 100%;
-  }
+    .title {
+      font-size: 28px;
+      line-height: 28px;
+      margin-top: 21px;
+      margin-bottom: 13px;
+      font-weight: bold;
+      color: ${getColor('darkPurple')};
+    }
 
-  .button {
-    display: inline-flex;
-    padding: 12px 24px;
-    border-radius: 40px;
-    background-color: ${getColor('darkPurple')};
+    .buttonContainer {
+      position: absolute;
+      bottom: 59px;
+      width: 100%;
+    }
 
-    transition: all 0.15s ease;
+    .button {
+      display: inline-flex;
+      padding: 12px 24px;
+      border-radius: 40px;
+      background-color: ${getColor('darkPurple')};
+
+      transition: all 0.15s ease;
+
+      span {
+        align-self: center;
+
+        font-weight: bold;
+        font-size: 18px;
+        line-height: 22px;
+        letter-spacing: 0.5px;
+
+        color: #ffffff;
+        text-decoration: none;
+      }
+    }
+
+    .button:hover {
+      transform: translateY(-1px);
+    }
+  `;
+
+  return props => {
+    const { url } = useCurrentRoute();
+    const { lang } = useLanguage();
+
+    return (
+      <CardsContainer {...props}>
+        <Card
+          style={{
+            background:
+              'radial-gradient(111.67% 100% at 0% 0%, #F2FFE6 0%, #C6FFF9 100%)'
+          }}
+        >
+          <TradeIcon />
+          <h1 className="title">{lang.landing_page.trade_card.title}</h1>
+          <Text t="body">{lang.landing_page.trade_card.description}</Text>
+          <div className="buttonContainer">
+            <Link
+              href={`/${Routes.TRADE}`}
+              className="button"
+              onClick={() => {
+                mixpanel.track('btn-click', {
+                  id: 'StartTrading',
+                  product: 'oasis-landing'
+                });
+              }}
+            >
+              <span>{lang.landing_page.trade_card.button}</span>
+            </Link>
+          </div>
+        </Card>
+        <Card
+          style={{
+            background:
+              'radial-gradient(100% 100% at 0% 0%, #E2FFCC 0%, #FFF1CF 100%)'
+          }}
+        >
+          <BorrowIcon />
+          <h1 className="title">{lang.landing_page.borrow_card.title}</h1>
+          <Text t="body">{lang.landing_page.borrow_card.description}</Text>
+          <div className="buttonContainer">
+            <Link
+              href={`/${Routes.BORROW}${url.search}`}
+              prefetch={true}
+              className="button"
+              onClick={() => {
+                mixpanel.track('btn-click', {
+                  id: 'BorrowDai',
+                  product: 'oasis-landing'
+                });
+              }}
+            >
+              <span>{lang.landing_page.borrow_card.button}</span>
+            </Link>
+          </div>
+        </Card>
+        <Card
+          style={{
+            background:
+              'radial-gradient(100% 100% at 0% 0%, #FFE9E9 0%, #FFE9B5 100%)',
+            marginBottom: 0
+          }}
+        >
+          <SaveIcon />
+          <h1 className="title">{lang.landing_page.save_card.title}</h1>
+          <Text t="body">{lang.landing_page.save_card.description}</Text>
+          <div className="buttonContainer">
+            <Link
+              href={`/${Routes.SAVE}${url.search}`}
+              prefetch={true}
+              className="button"
+              onClick={() => {
+                mixpanel.track('btn-click', {
+                  id: 'SaveDai',
+                  product: 'oasis-landing'
+                });
+              }}
+            >
+              <span>{lang.landing_page.save_card.button}</span>
+            </Link>
+          </div>
+        </Card>
+      </CardsContainer>
+    );
+  };
+})();
+
+const SupportedTokens = (() => {
+  const tokens = [
+    {
+      name: 'Dai',
+      icon: DaiIcon
+    },
+    {
+      name: 'Ethereum',
+      icon: EthIcon
+    },
+    {
+      name: 'BAT',
+      icon: BatIcon
+    },
+    {
+      name: 'Augur',
+      icon: RepIcon,
+      onlyOnTrade: true
+    },
+    {
+      name: 'USDC',
+      icon: UsdcIcon
+    },
+    {
+      name: '0x',
+      icon: ZrxIcon,
+      onlyOnTrade: true
+    }
+  ];
+
+  const TokenList = styled.div`
+    max-width: 1170px;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    margin: 74px auto 69px;
+  `;
+
+  const TokenStyle = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 70px;
+
+    svg {
+      flex-grow: 0;
+      flex-shrink: 0;
+    }
+
+    svg {
+      width: 70px;
+    }
 
     span {
-      align-self: center;
-
-      font-weight: bold;
-      font-size: 18px;
-      line-height: 22px;
+      margin-top: 28px;
+      font-size: 22px;
       letter-spacing: 0.5px;
-
-      color: #ffffff;
-      text-decoration: none;
+      color: ${getColor('darkPurple')};
+      position: relative;
     }
-  }
 
-  .button:hover {
-    transform: translateY(-1px);
-  }
-`;
+    span.onlyOnTrade:after {
+      font-family: 'Arial Hebrew', Arial, sans-serif;
+      content: '*';
+      font-size: 34px;
+      position: absolute;
+      top: 1rem;
+      line-height: 20px;
+    }
+  `;
 
-const tokens = [
-  {
-    name: 'Dai',
-    icon: DaiIcon
-  },
-  {
-    name: 'Ethereum',
-    icon: EthIcon
-  },
-  {
-    name: 'BAT',
-    icon: BatIcon
-  },
-  {
-    name: 'Augur',
-    icon: RepIcon,
-    onlyOnTrade: true
-  },
-  {
-    name: 'USDC',
-    icon: UsdcIcon
-  },
-  {
-    name: '0x',
-    icon: ZrxIcon,
-    onlyOnTrade: true
-  }
-];
+  const Token = ({ config: { name, icon, onlyOnTrade } }) => {
+    const Icon = icon;
+    return (
+      <TokenStyle>
+        <Icon />
+        <span className={onlyOnTrade ? 'onlyOnTrade' : ''}>{name}</span>
+      </TokenStyle>
+    );
+  };
 
-const TokenList = styled.div`
-  max-width: 1170px;
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-  margin: 74px auto 69px;
-`;
+  return props => {
+    const { lang } = useLanguage();
 
-const TokenStyle = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 70px;
-
-  svg {
-    flex-grow: 0;
-    flex-shrink: 0;
-  }
-
-  svg {
-    width: 70px;
-  }
-
-  span {
-    margin-top: 28px;
-    font-size: 22px;
-    letter-spacing: 0.5px;
-    color: ${getColor('darkPurple')};
-    position: relative;
-  }
-
-  span.onlyOnTrade:after {
-    font-family: 'Arial Hebrew', Arial, sans-serif;
-    content: '*';
-    font-size: 34px;
-    position: absolute;
-    top: 1rem;
-    line-height: 20px;
-  }
-`;
-
-const Token = ({ config: { name, icon, onlyOnTrade } }) => {
-  const Icon = icon;
-  return (
-    <TokenStyle>
-      <Icon />
-      <span className={onlyOnTrade ? 'onlyOnTrade' : ''}>{name}</span>
-    </TokenStyle>
-  );
-};
+    return (
+      <Box {...props}>
+        <Text t="h2">{lang.landing_page.token_section_title}</Text>
+        <TokenList>
+          {tokens.map(config => (
+            <Token config={config} key={config.name} />
+          ))}
+        </TokenList>
+        <span
+          style={{
+            fontSize: '18px',
+            letterSpacing: '0.5px',
+            color: '#4F445E',
+            position: 'relative'
+          }}
+        >
+          <span
+            style={{ fontSize: '2.6rem', position: 'relative', top: '5px' }}
+          >
+            *
+          </span>{' '}
+          {lang.landing_page.token_section_only_on_trade}
+        </span>
+      </Box>
+    );
+  };
+})();
 
 const BlurryBackground = (() => {
   const ballsContainerOriginalWidth = 1440;
@@ -269,28 +389,56 @@ const BlurryBackground = (() => {
   );
 })();
 
-const JumboBlock = styled(Box)`
-  max-width: 966px;
-  background: radial-gradient(100% 100% at 0% 0%, #f4ffec 0%, #fef4db 100%);
-  text-align: left;
-  padding: 131px 13% 122px 12%;
+const BulletPoints = (() => {
+  const JumboBlock = styled(Box)`
+    max-width: 966px;
+    background: radial-gradient(100% 100% at 0% 0%, #f4ffec 0%, #fef4db 100%);
+    text-align: left;
+    padding: 131px 13% 122px 12%;
 
-  @media (max-width: ${props => props.theme.breakpoints.m}) {
-    padding-top: 104px;
-    padding-bottom: 97px;
-  }
+    @media (max-width: ${props => props.theme.breakpoints.m}) {
+      padding-top: 104px;
+      padding-bottom: 97px;
+    }
 
-  & > div:not(:first-child) {
-    margin-top: 121px;
-  }
+    & > div:not(:first-child) {
+      margin-top: 121px;
+    }
 
-  .title {
-    margin-bottom: 23px;
-  }
-`;
+    .title {
+      margin-bottom: 23px;
+    }
+  `;
+
+  return props => {
+    const { lang } = useLanguage();
+
+    return (
+      <JumboBlock mr={{ s: 0, xl: '35px' }} {...props}>
+        <div>
+          <Text t="h3" className="title">
+            {lang.landing_page.section1_title}
+          </Text>
+          <Text t="body">{lang.landing_page.section1_p}</Text>
+        </div>
+        <div>
+          <Text t="h3" className="title">
+            {lang.landing_page.section2_title}
+          </Text>
+          <Text t="body">{lang.landing_page.section2_p}</Text>
+        </div>
+        <div>
+          <Text t="h3" className="title">
+            {lang.landing_page.section3_title}
+          </Text>
+          <Text t="body">{lang.landing_page.section3_p}</Text>
+        </div>
+      </JumboBlock>
+    );
+  };
+})();
 
 function Landing() {
-  const { url } = useCurrentRoute();
   const { lang } = useLanguage();
 
   return (
@@ -300,131 +448,14 @@ function Landing() {
         <Box mt="149px">
           <Text t="h1">{lang.landing_page.headline}</Text>
         </Box>
-        <Cards>
-          <Card
-            style={{
-              background:
-                'radial-gradient(111.67% 100% at 0% 0%, #F2FFE6 0%, #C6FFF9 100%)'
-            }}
-          >
-            <TradeIcon />
-            <h1 className="title">{lang.landing_page.trade_card.title}</h1>
-            <Text t="body">{lang.landing_page.trade_card.description}</Text>
-            <div className="buttonContainer">
-              <Link
-                href={`/${Routes.TRADE}`}
-                className="button"
-                onClick={() => {
-                  mixpanel.track('btn-click', {
-                    id: 'StartTrading',
-                    product: 'oasis-landing'
-                  });
-                }}
-              >
-                <span>{lang.landing_page.trade_card.button}</span>
-              </Link>
-            </div>
-          </Card>
-          <Card
-            style={{
-              background:
-                'radial-gradient(100% 100% at 0% 0%, #E2FFCC 0%, #FFF1CF 100%)'
-            }}
-          >
-            <BorrowIcon />
-            <h1 className="title">{lang.landing_page.borrow_card.title}</h1>
-            <Text t="body">{lang.landing_page.borrow_card.description}</Text>
-            <div className="buttonContainer">
-              <Link
-                href={`/${Routes.BORROW}${url.search}`}
-                prefetch={true}
-                className="button"
-                onClick={() => {
-                  mixpanel.track('btn-click', {
-                    id: 'BorrowDai',
-                    product: 'oasis-landing'
-                  });
-                }}
-              >
-                <span>{lang.landing_page.borrow_card.button}</span>
-              </Link>
-            </div>
-          </Card>
-          <Card
-            style={{
-              background:
-                'radial-gradient(100% 100% at 0% 0%, #FFE9E9 0%, #FFE9B5 100%)',
-              marginBottom: 0
-            }}
-          >
-            <SaveIcon />
-            <h1 className="title">{lang.landing_page.save_card.title}</h1>
-            <Text t="body">{lang.landing_page.save_card.description}</Text>
-            <div className="buttonContainer">
-              <Link
-                href={`/${Routes.SAVE}${url.search}`}
-                prefetch={true}
-                className="button"
-                onClick={() => {
-                  mixpanel.track('btn-click', {
-                    id: 'SaveDai',
-                    product: 'oasis-landing'
-                  });
-                }}
-              >
-                <span>{lang.landing_page.save_card.button}</span>
-              </Link>
-            </div>
-          </Card>
-        </Cards>
-        <Box style={{ marginTop: '103px' }}>
-          <Text t="h2">{lang.landing_page.token_section_title}</Text>
-          <TokenList>
-            {tokens.map(config => (
-              <Token config={config} key={config.name} />
-            ))}
-          </TokenList>
-          <span
-            style={{
-              fontSize: '18px',
-              letterSpacing: '0.5px',
-              color: '#4F445E',
-              position: 'relative'
-            }}
-          >
-            <span
-              style={{ fontSize: '2.6rem', position: 'relative', top: '5px' }}
-            >
-              *
-            </span>{' '}
-            {lang.landing_page.token_section_only_on_trade}
-          </span>
-        </Box>
+        <Cards mt="80px" />
+        <SupportedTokens mt="103px" />
       </Content>
       <Box style={{ position: 'relative', width: '100%', marginTop: '207px' }}>
         <BlurryBackground />
         <Content>
           <Box m="0 auto" display="inline-block">
-            <JumboBlock mr={{ s: 0, xl: '35px' }}>
-              <div>
-                <Text t="h3" className="title">
-                  {lang.landing_page.section1_title}
-                </Text>
-                <Text t="body">{lang.landing_page.section1_p}</Text>
-              </div>
-              <div>
-                <Text t="h3" className="title">
-                  {lang.landing_page.section2_title}
-                </Text>
-                <Text t="body">{lang.landing_page.section2_p}</Text>
-              </div>
-              <div>
-                <Text t="h3" className="title">
-                  {lang.landing_page.section3_title}
-                </Text>
-                <Text t="body">{lang.landing_page.section3_p}</Text>
-              </div>
-            </JumboBlock>
+            <BulletPoints />
           </Box>
         </Content>
       </Box>
