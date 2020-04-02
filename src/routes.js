57b2d6770eee9a01b8cde0862ad72313560c13ee
Fernando Tiberti <fertiberti@gmail.com>
2020-04-01 23:21:34 -0300

    Revert bad merge for routes.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
-import { map, route, mount, withView } from 'navi';
+import { map, route, mount, withView, compose } from 'navi';
 import { View } from 'react-navi';
 
 import Navbar from 'components/Navbar';
 import DashboardLayout from 'layouts/DashboardLayout';
+import MarketingLayout from './layouts/MarketingLayout';
 import Landing from 'pages/Landing';
 import Overview from 'pages/Overview';
 import Borrow from 'pages/Borrow';
@@ -27,54 +28,68 @@ import { Routes } from 'utils/constants';
 
 const { networkNames, defaultNetwork } = config;
 
-const withDashboardLayout = route =>
-  withView(async request => {
-    const {
-      network = networkNames[defaultNetwork],
-      testchainId,
-      backendEnv
-    } = request.query;
-    const { viewedAddress, cdpId } = request.params;
+const dappProvidersView = async request => {
+  const {
+    network = networkNames[defaultNetwork],
+    testchainId,
+    backendEnv
+  } = request.query;
+  const { viewedAddress } = request.params;
 
-    return (
-      <MakerProvider
-        network={network}
-        testchainId={testchainId}
-        backendEnv={backendEnv}
-        viewedAddress={viewedAddress}
-      >
-        <RouteEffects network={network} />
-        <TransactionManagerProvider>
-          <NotificationProvider>
-            <VaultsProvider viewedAddress={viewedAddress}>
-              <ToggleProvider>
-                <ModalProvider modals={modals} templates={templates}>
-                  <SidebarProvider>
-                    <DashboardLayout
-                      mobileNav={
-                        <MobileNav
-                          viewedAddress={viewedAddress}
-                          cdpId={cdpId}
-                        />
-                      }
-                      navbar={<Navbar viewedAddress={viewedAddress} />}
-                    >
-                      <View />
-                    </DashboardLayout>
-                  </SidebarProvider>
-                </ModalProvider>
-              </ToggleProvider>
-            </VaultsProvider>
-          </NotificationProvider>
-        </TransactionManagerProvider>
-      </MakerProvider>
-    );
-  }, route);
+  return (
+    <MakerProvider
+      network={network}
+      testchainId={testchainId}
+      backendEnv={backendEnv}
+      viewedAddress={viewedAddress}
+    >
+      <RouteEffects network={network} />
+      <TransactionManagerProvider>
+        <NotificationProvider>
+          <VaultsProvider viewedAddress={viewedAddress}>
+            <ToggleProvider>
+              <ModalProvider modals={modals} templates={templates}>
+                <SidebarProvider>
+                  <View />
+                </SidebarProvider>
+              </ModalProvider>
+            </ToggleProvider>
+          </VaultsProvider>
+        </NotificationProvider>
+      </TransactionManagerProvider>
+    </MakerProvider>
+  );
+};
+
+const withDashboardLayout = childMatcher =>
+  compose(
+    withView(dappProvidersView),
+    withView(request => {
+      const { viewedAddress, cdpId } = request.params;
+      return (
+        <DashboardLayout
+          mobileNav={<MobileNav viewedAddress={viewedAddress} cdpId={cdpId} />}
+          navbar={<Navbar viewedAddress={viewedAddress} />}
+        >
+          <View />
+        </DashboardLayout>
+      );
+    }),
+    childMatcher
+  );
+
+const marketingLayoutView = () => (
+  <MarketingLayout>
+    <View />
+  </MarketingLayout>
+);
 
 export default mount({
   '/': route(() => ({ title: 'Landing', view: <Landing /> })),
 
-  [`/${Routes.BORROW}`]: withDashboardLayout(
+  [`/${Routes.BORROW}`]: compose(
+    withView(dappProvidersView),
+    withView(marketingLayoutView),
     route(() => ({ title: 'Borrow', view: <Borrow /> }))
   ),
 
@@ -99,7 +114,9 @@ export default mount({
     })
   ),
 
-  [`/${Routes.SAVE}`]: withDashboardLayout(
+  [`/${Routes.SAVE}`]: compose(
+    withView(dappProvidersView),
+    withView(marketingLayoutView),
     route(() => ({ title: 'Save', view: <SaveOverview /> }))
   ),
 
