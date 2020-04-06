e789af9cb0f8cfd3a3357f53f2ffb6231183800a
Fernando Tiberti <fertiberti@gmail.com>
2020-04-06 17:59:51 -0300

    Instantiate ScrollManager inside useEffect.

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -4,9 +4,8 @@ import ScrollManager from 'window-scroll-manager';
 const useScroll = () => {
   const [scroll, setScroll] = useState(0);
 
-  new ScrollManager();
-
   useEffect(() => {
+    new ScrollManager();
     window.addEventListener('window-scroll', scrollHandler);
     return () => {
       window.removeEventListener('window-scroll', scrollHandler);
