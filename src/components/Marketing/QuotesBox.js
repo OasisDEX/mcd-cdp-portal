f1a68e502e2855991f7a3b3b13c4b3d11c4b0b87
Fernando Tiberti <fertiberti@gmail.com>
2020-04-09 15:57:46 -0300

    Slight refactoring.

--- src/components/Marketing/QuotesBox.js
+++ src/components/Marketing/QuotesBox.js
@@ -5,73 +5,71 @@ import { Box, Text } from '@makerdao/ui-components-core';
 import { TextBlock } from '../Typography';
 import { Link } from 'react-navi';
 
-const QuotesBox = (() => {
-  const QuotesBoxStyle = styled(Box)`
-    background: gray;
-    max-width: 980px;
-    padding: 100px 9.1% 76px;
-    margin: 0 auto;
-    position: relative;
-  `;
+const QuotesBoxStyle = styled(Box)`
+  background: gray;
+  max-width: 980px;
+  padding: 100px 9.1% 76px;
+  margin: 0 auto;
+  position: relative;
+`;
 
-  const QuoteWrapper = styled(Box)`
-    display: grid;
-    grid-template-columns: 50px auto 50px;
-    grid-column-gap: 7px;
-    max-width: 760px;
-    margin: 48px auto;
-  `;
+const QuoteWrapper = styled(Box)`
+  display: grid;
+  grid-template-columns: 50px auto 50px;
+  grid-column-gap: 7px;
+  max-width: 760px;
+  margin: 48px auto;
+`;
 
-  const QuotesImgWrapper = styled.div`
-    padding-top: 10px;
-    justify-self: left;
-  `;
+const QuotesImgWrapper = styled.div`
+  padding-top: 10px;
+  justify-self: left;
+`;
 
-  const Quote = styled(TextBlock)`
-    font-weight: bold;
-    font-size: 19px;
-    line-height: 30px;
-  `;
+const Quote = styled(TextBlock)`
+  font-weight: bold;
+  font-size: 19px;
+  line-height: 30px;
+`;
 
-  const Author = styled(Text)`
-    margin-top: 16px;
-    font-size: 19px;
-    .name {
-      text-decoration: underline;
-    }
-  `;
+const Author = styled(Text)`
+  margin-top: 16px;
+  font-size: 19px;
+  .name {
+    text-decoration: underline;
+  }
+`;
 
-  return ({
-    title,
-    body,
-    quote,
-    author,
-    url,
-    children,
-    quotesImg,
-    ...props
-  }) => {
-    return (
-      <QuotesBoxStyle {...props}>
-        <Text.h2 mb="16px">{title}</Text.h2>
-        <Text>{body}</Text>
-        <QuoteWrapper>
-          <QuotesImgWrapper>{quotesImg}</QuotesImgWrapper>
-          <div>
-            <Quote>“{quote}”</Quote>
-            <Author as="div">
-              <span>— </span>
-              <Link className="name" href={url}>
-                {author}
-              </Link>
-            </Author>
-          </div>
-          <div />
-        </QuoteWrapper>
-        {children}
-      </QuotesBoxStyle>
-    );
-  };
-})();
+const QuotesBox = ({
+  title,
+  body,
+  quote,
+  author,
+  url,
+  children,
+  quotesImg,
+  ...props
+}) => {
+  return (
+    <QuotesBoxStyle {...props}>
+      <Text.h2 mb="16px">{title}</Text.h2>
+      <Text>{body}</Text>
+      <QuoteWrapper>
+        <QuotesImgWrapper>{quotesImg}</QuotesImgWrapper>
+        <div>
+          <Quote>“{quote}”</Quote>
+          <Author as="div">
+            <span>— </span>
+            <Link className="name" href={url}>
+              {author}
+            </Link>
+          </Author>
+        </div>
+        <div />
+      </QuoteWrapper>
+      {children}
+    </QuotesBoxStyle>
+  );
+};
 
 export default hot(QuotesBox);
