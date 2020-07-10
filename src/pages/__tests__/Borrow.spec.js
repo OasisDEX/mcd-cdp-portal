a7d247e0814a2e409a296eedd66be311bec7236e
Fernando Tiberti <fertiberti@gmail.com>
2020-07-10 00:55:04 -0300

    Fix formatting.

--- src/pages/__tests__/Borrow.spec.js
+++ src/pages/__tests__/Borrow.spec.js
@@ -7,17 +7,18 @@ import useAnalytics from 'hooks/useAnalytics';
 jest.mock('hooks/useAnalytics');
 useAnalytics.mockReturnValue(jest.fn());
 
-
 test('Borrow calculator renders', async () => {
-  const { getByTestId } = await renderWithMaker(<Borrow disableConnect={true} />, {
-    waitForAuth: false
-  });
+  const { getByTestId } = await renderWithMaker(
+    <Borrow disableConnect={true} />,
+    {
+      waitForAuth: false
+    }
+  );
 
-  const amountChosenElem = await waitForElement(
-    () => getByTestId('amount-chosen')
+  const amountChosenElem = await waitForElement(() =>
+    getByTestId('amount-chosen')
   );
 
   // 25 ETH is the default amount in the Borrow calculator
   expect(amountChosenElem.innerHTML.includes('25')).toBeTruthy();
-
 });
