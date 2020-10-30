aa8b6f379a732ec068e51fc94d3d2ad9bc9147a6
Fernando Tiberti <fertiberti@gmail.com>
2020-10-30 13:01:25 -0300

    Fix BorrowWBTCLanding not having a layout.

--- src/pages/BorrowWBTCLanding.js
+++ src/pages/BorrowWBTCLanding.js
@@ -23,6 +23,7 @@ import { ReactComponent as Step2Img } from 'images/landing/borrow-wbtc/step2.svg
 import { ReactComponent as Step3Img } from 'images/landing/borrow-wbtc/step3.svg';
 import { ReactComponent as Step4Img } from 'images/landing/borrow-wbtc/step4.svg';
 import { ReactComponent as Step5Img } from 'images/landing/borrow-wbtc/step5.svg';
+import MarketingLayout from 'layouts/MarketingLayout';
 
 const StepNumber = styled(Box)`
   background: #ffeec5;
@@ -209,122 +210,124 @@ function BorrowWBTCLanding() {
   }, [account, navigation]);
 
   return (
-    <StyledPageContentLayout>
-      <FixedHeaderTrigger>
-        <HeroStyle>
-          <Box className="content">
-            <ThickUnderline background="linear-gradient(176.36deg, #FFE9E9 26.84%, #FFDB87 97.79%)">
-              <Text.h4>{lang.borrow_landing.page_name}</Text.h4>
-            </ThickUnderline>
-            <Text.h1 className="headline">
-              {lang.borrow_wbtc_landing.headline}
-            </Text.h1>
-            <Box className="subheadline">
-              <Text>{lang.borrow_wbtc_landing.subheadline}</Text>
-            </Box>
-            <Text fontSize="s" className="connect-to-start">
-              {lang.borrow_landing.connect_to_start}
-            </Text>
-            <AccountSelection className="button" mt="27px" />
-          </Box>
-          <Box className="img">
-            <MainImg />
-          </Box>
-        </HeroStyle>
-      </FixedHeaderTrigger>
-      <Box maxWidth="790px" m="180px auto">
-        <Text.h2 style={{ marginBottom: '20px' }}>
-          {lang.borrow_wbtc_landing.about_title}
-        </Text.h2>
-        <TextBlock>{lang.borrow_wbtc_landing.about_content}</TextBlock>
-        <Text>
-          {lang.formatString(lang.borrow_wbtc_landing.about_learn_more, {
-            link: (
-              <ExternalLink href="https://www.wbtc.network/">
-                https://www.wbtc.network/
-              </ExternalLink>
-            )
-          })}
-        </Text>
-      </Box>
-      <Box>
-        <Step
-          number={1}
-          title={lang.borrow_wbtc_landing.step1}
-          details={lang.formatString(lang.borrow_wbtc_landing.step1_details, {
-            link: (
-              <ExternalLink href="https://coinlist.co/wbtc-signup?referral_code=FETRCM">
-                CoinList
-              </ExternalLink>
-            )
-          })}
-          Img={Step1Img}
-        />
-        <Step
-          number={2}
-          title={lang.borrow_wbtc_landing.step2}
-          details={lang.borrow_wbtc_landing.step2_details}
-          Img={Step2Img}
-        />
-        <Step
-          number={3}
-          title={lang.borrow_wbtc_landing.step3}
-          details={lang.borrow_wbtc_landing.step3_details}
-          Img={Step3Img}
-        />
-        <Step
-          number={4}
-          title={lang.borrow_wbtc_landing.step4}
-          details={lang.borrow_wbtc_landing.step4_details}
-          Img={Step4Img}
-        />
-        <Step
-          number={5}
-          title={lang.borrow_wbtc_landing.step5}
-          details={lang.formatString(lang.borrow_wbtc_landing.step5_details, {
-            link: (
-              <Link
-                style={{ textDecoration: 'underline' }}
-                href={`/${Routes.BORROW}`}
-              >
-                {lang.borrow_wbtc_landing.step5_link_text}
-              </Link>
-            )
-          })}
-          Img={Step5Img}
-        />
-      </Box>
-      <QuestionsWrapper mt={{ s: '159px', m: '250px' }}>
-        <Text.h2>{lang.landing_page.questions_title}</Text.h2>
-        <Questions
-          questions={buildQuestionsFromLangObj(
-            lang.borrow_landing.questions,
-            lang
-          )}
-          links={
-            <>
-              <Link
-                href="https://community-development.makerdao.com/makerdao-mcd-faqs/faqs/vault"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {lang.borrow_landing.questions.bottom_link1}
-              </Link>
-              <Box display={{ s: 'none', m: 'inline-block' }}>
-                <SeparatorDot mx="24px" />
+    <MarketingLayout showNavInFooter={true}>
+      <StyledPageContentLayout>
+        <FixedHeaderTrigger>
+          <HeroStyle>
+            <Box className="content">
+              <ThickUnderline background="linear-gradient(176.36deg, #FFE9E9 26.84%, #FFDB87 97.79%)">
+                <Text.h4>{lang.borrow_landing.page_name}</Text.h4>
+              </ThickUnderline>
+              <Text.h1 className="headline">
+                {lang.borrow_wbtc_landing.headline}
+              </Text.h1>
+              <Box className="subheadline">
+                <Text>{lang.borrow_wbtc_landing.subheadline}</Text>
               </Box>
-              <Link
-                href="https://community-development.makerdao.com/makerdao-mcd-faqs/faqs/glossary"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {lang.borrow_landing.questions.bottom_link2}
-              </Link>
-            </>
-          }
-        />
-      </QuestionsWrapper>
-    </StyledPageContentLayout>
+              <Text fontSize="s" className="connect-to-start">
+                {lang.borrow_landing.connect_to_start}
+              </Text>
+              <AccountSelection className="button" mt="27px" />
+            </Box>
+            <Box className="img">
+              <MainImg />
+            </Box>
+          </HeroStyle>
+        </FixedHeaderTrigger>
+        <Box maxWidth="790px" m="180px auto">
+          <Text.h2 style={{ marginBottom: '20px' }}>
+            {lang.borrow_wbtc_landing.about_title}
+          </Text.h2>
+          <TextBlock>{lang.borrow_wbtc_landing.about_content}</TextBlock>
+          <Text>
+            {lang.formatString(lang.borrow_wbtc_landing.about_learn_more, {
+              link: (
+                <ExternalLink href="https://www.wbtc.network/">
+                  https://www.wbtc.network/
+                </ExternalLink>
+              )
+            })}
+          </Text>
+        </Box>
+        <Box>
+          <Step
+            number={1}
+            title={lang.borrow_wbtc_landing.step1}
+            details={lang.formatString(lang.borrow_wbtc_landing.step1_details, {
+              link: (
+                <ExternalLink href="https://coinlist.co/wbtc-signup?referral_code=FETRCM">
+                  CoinList
+                </ExternalLink>
+              )
+            })}
+            Img={Step1Img}
+          />
+          <Step
+            number={2}
+            title={lang.borrow_wbtc_landing.step2}
+            details={lang.borrow_wbtc_landing.step2_details}
+            Img={Step2Img}
+          />
+          <Step
+            number={3}
+            title={lang.borrow_wbtc_landing.step3}
+            details={lang.borrow_wbtc_landing.step3_details}
+            Img={Step3Img}
+          />
+          <Step
+            number={4}
+            title={lang.borrow_wbtc_landing.step4}
+            details={lang.borrow_wbtc_landing.step4_details}
+            Img={Step4Img}
+          />
+          <Step
+            number={5}
+            title={lang.borrow_wbtc_landing.step5}
+            details={lang.formatString(lang.borrow_wbtc_landing.step5_details, {
+              link: (
+                <Link
+                  style={{ textDecoration: 'underline' }}
+                  href={`/${Routes.BORROW}`}
+                >
+                  {lang.borrow_wbtc_landing.step5_link_text}
+                </Link>
+              )
+            })}
+            Img={Step5Img}
+          />
+        </Box>
+        <QuestionsWrapper mt={{ s: '159px', m: '250px' }}>
+          <Text.h2>{lang.landing_page.questions_title}</Text.h2>
+          <Questions
+            questions={buildQuestionsFromLangObj(
+              lang.borrow_landing.questions,
+              lang
+            )}
+            links={
+              <>
+                <Link
+                  href="https://community-development.makerdao.com/makerdao-mcd-faqs/faqs/vault"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {lang.borrow_landing.questions.bottom_link1}
+                </Link>
+                <Box display={{ s: 'none', m: 'inline-block' }}>
+                  <SeparatorDot mx="24px" />
+                </Box>
+                <Link
+                  href="https://community-development.makerdao.com/makerdao-mcd-faqs/faqs/glossary"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {lang.borrow_landing.questions.bottom_link2}
+                </Link>
+              </>
+            }
+          />
+        </QuestionsWrapper>
+      </StyledPageContentLayout>
+    </MarketingLayout>
   );
 }
 
