b79c6a07100b855f94b5dfe0d569b2aef10a90f4
Fernando Tiberti <fertiberti@gmail.com>
2021-01-12 13:11:41 -0300

    Change Twitter meta img content to "summary_large_image".

--- src/components/Marketing/PageHead.js
+++ src/components/Marketing/PageHead.js
@@ -6,7 +6,7 @@ const PageHead = ({ title, description, imgUrl }) => (
     <title>{title}</title>
     <meta name="title" content={title} />
     <meta name="description" content={description} />
-    <meta name="twitter:card" value="summary" />
+    <meta name="twitter:card" content="summary_large_image" />
     <meta property="og:title" content={title} />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="http://oasis.app" />
